6c2441519903f6b69806cb947b76148f9c4b8ec2
ericklhg22 <73855575+ericklhg22@users.noreply.github.com>
2020-11-02 17:24:27 -0400

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1604349881,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/block.png",
+		"images/player-sheet0.png",
+		"media/explosion.m4a",
+		"media/explosion.ogg",
+		"media/jigglypuff (usa) 2.m4a",
+		"media/jigglypuff (usa) 2.ogg",
+		"media/canción de saria-192k.m4a",
+		"media/canción de saria-192k.ogg",
+		"media/22222.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
